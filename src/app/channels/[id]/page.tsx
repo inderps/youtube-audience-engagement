eb4dc54c880898e64cc7d05a20ab400c489c775e
Inderpal Singh <inderpal.singh@lnds.lu>
2024-02-17 23:29:42 +0100

    add top video request

--- src/app/channels/[id]/page.tsx
+++ src/app/channels/[id]/page.tsx
@@ -13,17 +13,33 @@ export default async function Page({ params }: PageProps) {
     parseInt(params.id),
   );
 
+  // answers = "suggestion1, suggestion 2, suggestion 3"
+  const answers = await openaiService.analyzeTopVideoRequests(
+    parseInt(params.id),
+  );
+
+  const suggestions = answers.split(', ').filter(Boolean);
+
   return (
     <div className="dark:bg-gray-800 dark:text-gray-200 p-4">
-      <div className="flex justify-center items-center">
-        <div className="bg-gray-700 p-4 rounded-lg shadow-lg w-1/2">
+      <div className="flex justify-center items-start gap-4 flex-row">
+        <div className="bg-gray-700 p-4 rounded-lg shadow-lg w-full md:w-1/2">
           <h2 className="text-xl font-semibold mb-4">
             Sentiment Analysis for overall channel
           </h2>
+          {result && <SentimentAnalysisPieChart sentimentAnalysis={result} />}
           {result?.content_quality && (
             <p className="mt-4">{result.content_quality}</p>
           )}
-          <SentimentAnalysisPieChart sentimentAnalysis={result} />
+        </div>
+
+        <div className="bg-gray-700 p-4 rounded-lg shadow-lg w-full md:w-1/2">
+          <h2 className="text-xl font-semibold mb-4">Top Video Requests</h2>
+          <ul className="list-disc pl-5">
+            {suggestions.map((suggestion, index) => (
+              <li key={index}>{suggestion}</li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
--- src/app/services/openaiService.ts
+++ src/app/services/openaiService.ts
@@ -27,6 +27,29 @@ export default class OpenaiService {
   ): Promise<SentimentAnalysisResult | undefined> {
     // return new Promise(async (resolve, reject) => (resolve({ positive: 100, negative: 0, neutral: 0, content_quality: 'Audience values the content for being great, loved, and very informative.' })));
 
+    const message =
+      "You are a youtube video comments analyzer for a whole channel. Summarize the main concerns and overall feelings of the audience about the videos based on the comments provided below, focusing on the content creator's value. Provide the overall feelings of the audience in percentages (positive, negative, neutral), excluding negative sentiments not related to content quality. The summary should be in JSON format, including counts of positive, negative, and neutral sentiments Eg: { positive: 100, negative: 0, neutral: 0 content_quality: 'Audience values the content for being great, loved, and very informative.' }. Do not include ``json quotes in your response. Also keys should be under quotes. I will directly do JSON.parse(answer) to your answer. Exclude comments that are not related to the content creator's value. Don't count them";
+
+    const responseText = await this.ask(channelId, message);
+
+    if (!responseText) {
+      return;
+    }
+
+    const analysisResult: SentimentAnalysisResult = JSON.parse(responseText);
+    return analysisResult;
+  }
+
+  async analyzeTopVideoRequests(channelId: number): Promise<string> {
+    // return new Promise(async (resolve, reject) => (resolve({ positive: 100, negative: 0, neutral: 0, content_quality: 'Audience values the content for being great, loved, and very informative.' })));
+
+    const message =
+      "You are a youtube video comments analyzer for a whole channel. Given the following comments from a YouTube channel, identify and return a comma-separated list of the top 3 most requested videos or topics that the audience wants from the content creator. Ensure these are requests directed towards the content creator for future content. If there is no direct request, analyse the comments and suggest based on the discussions, what they might want as next topic. Do mention it if its directly asked or you analysed based on discussions. Response should be in format: Eg: 'suggestion 1, 'suggestion 2', 'suggestion 3'";
+
+    return (await this.ask(channelId, message)) || '';
+  }
+
+  async ask(channelId: number, message: string): Promise<string | undefined> {
     const channel = await Channel.findByPk(channelId, {
       include: [
         {
@@ -47,21 +70,15 @@ export default class OpenaiService {
       messages: [
         {
           role: 'system',
-          content:
-            "You are a youtube video comments analyzer for a whole channel. Summarize the main concerns and overall feelings of the audience about the videos based on the comments provided below, focusing on the content creator's value. Provide the overall feelings of the audience in percentages (positive, negative, neutral), excluding negative sentiments not related to content quality. The summary should be in JSON format, including counts of positive, negative, and neutral sentiments Eg: { positive: 100, negative: 0, neutral: 0 content_quality: 'Audience values the content for being great, loved, and very informative.' }. Do not include ``json quotes in your response. I will directly do JSON.parse(answer) to your answer. Exclude comments that are not related to the content creator's value. Don't count them",
+          content: message,
         },
         { role: 'user', content: combinedComments },
       ],
       model: 'gpt-3.5-turbo-0125',
     });
 
-    const responseText = completion.choices[0].message.content?.trim();
+    console.log('responseText', completion.choices[0].message.content); // eslint-disable-line no-console
 
-    if (!responseText) {
-      return;
-    }
-
-    const analysisResult: SentimentAnalysisResult = JSON.parse(responseText);
-    return analysisResult;
+    return completion.choices[0].message.content?.trim();
   }
 }
